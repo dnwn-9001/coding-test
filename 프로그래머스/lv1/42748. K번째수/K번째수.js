3880bee7f233b47cd2bdb2a8f6ffb29f7a77f7fd
Youngin <106906742+dnwn-9001@users.noreply.github.com>
2023-05-21 02:24:31 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -3,14 +3,7 @@ function solution(array, commands) {
     
     for (let i = 0; i < commands.length; i++) {
         let sliceArr = array.slice(commands[i][0] - 1, commands[i][1]);
-        for (let k = 0; k < sliceArr.length - 1; k++) {
-          let idx = k;
-          for (let j = k + 1; j < sliceArr.length; j++) {
-            if (sliceArr[j] < sliceArr[idx]) idx = j;
-          }
-          [sliceArr[k], sliceArr[idx]] = [sliceArr[idx], sliceArr[k]];
-        }
-
+        sliceArr.sort((a, b) => a - b);
         answer.push(sliceArr[commands[i][2] - 1]);
       }
     
